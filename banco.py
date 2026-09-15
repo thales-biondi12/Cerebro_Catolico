5416b25e0af613e4754cf0172340f418c27b5684
Thales Andrade Biondi <biodinho20@gmail.com>
2026-09-14 22:39:36 -0300

    Garante declaracao da variavel db em banco.py

--- banco.py
+++ banco.py
@@ -1,16 +1,14 @@
 import os
-from dotenv import load_dotenv
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-# Carrega as variáveis do arquivo .env
 load_dotenv()
 
+# Obtém a URI do ambiente
+MONGO_URI = os.getenv("MONGO_URI")
 
-def conectar_banco():
-    # Lê a URL do MongoDB a partir do .env ou usa o servidor local como fallback
-    mongo_uri = os.getenv("MONGO_URI")
-    client = MongoClient(mongo_uri)
+# Conecta ao cluster
+client = MongoClient(MONGO_URI)
 
-    # Nome do seu banco de dados
-    db = client["segundo_cerebro_catolico"]
-    return db["estudos"]
+# Define o banco de dados (A variável 'db' precisa ter exatamente este nome)
+db = client["segundo_cerebro"]  # Altere para o nome exato do seu banco no Atlas
