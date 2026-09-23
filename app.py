26731afc37cc28dec1622af15ed5eab7a3630221
Thales Andrade Biondi <biodinho20@gmail.com>
2026-09-22 21:31:41 -0300

    Ajusta deploy no Render

--- app.py
+++ app.py
@@ -304,4 +304,6 @@ def servir_arquivo(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(debug=debug, host="0.0.0.0", port=port)
